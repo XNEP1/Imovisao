a0ac4acad8539ffb823fbae002b4aa559f50a1fd
br42 <bruno.pontte@gmail.com>
2023-11-26 00:47:25 -0300

    Reverted classes Main and Sistema to their latest revisions on main branch

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Entrada entrada = Entrada.getInstance();
         Sistema sistema = new Sistema();
-        Cliente cliente = null;
         int opcao;
         System.out.println("+====================+");
         System.out.println("Bem-vindo ao Imovisão!");
         System.out.println("+====================+");
-        {
-            String nome, email, telefone, documento, endereco, senha1, senha2;
-            nome = entrada.leString("Digite seu nome:");
-            email = entrada.leString("Digite seu email:");
-            telefone = entrada.leString("Digite seu telefone:");
-            endereco = entrada.leString("Digite seu endereco:");
-            documento = entrada.leString("Digite seu documento (RG/CPF):");
-            do {
-                senha1 = entrada.leString("Digite sua senha:");
-                senha2 = entrada.leString("Confirme sua senha:");
-            } while (senha1 == null || senha2 == null || !senha1.equals(senha2) );
-            cliente = new Cliente(1, nome, email, telefone, senha1, documento,
-			new Endereco(endereco, 0));
-        }
         do {
             System.out.println("Menu:");
             System.out.println("1 - Ver produto");
@@ -38,23 +23,6 @@ public class Main {
                 break;
                 case 3:
                 System.out.println("Saindo...");
-                break;
-                case 99:
-                {
-                    Produto prod;
-                    int motivo;
-                    String mensagem, outro;
-                    idProduto = entrada.leLong("ID do produto");
-                    motivo = entrada.leInt("Motivo da denuncia");
-                    mensagem = entrada.leString("Mensagem da denuncia");
-                    outro = entrada.leString("Outro comentario (opcional)");
-                    prod = sistema.buscarProduto(idProduto);
-                    if (outro == "") {
-                        outro = null;
-                    }
-                    cliente.reportarAnuncio(prod, motivo, mensagem, outro);
-                    System.out.println("Denuncia registrada com sucesso.");
-                }
                 break;
                 default:
                 System.out.println("Opcao invalida.");
